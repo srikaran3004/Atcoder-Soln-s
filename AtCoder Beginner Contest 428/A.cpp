e141f3b6c762c7724ebd3187bfd99d66b7d6321a
Srikaran Devarakonda <srikaran2230@gmail.com>
2025-10-24 11:20:51 +0530

    Updated Sol of A!!

--- AtCoder Beginner Contest 428/A.cpp
+++ AtCoder Beginner Contest 428/A.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-//autor:@Srikaran
+// autor:@Srikaran
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
 void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
 #ifdef LOCAL
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
@@ -24,18 +38,26 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
 
-
-
-void solve() {
-    
+void solve()
+{
+    ll s, a, b, x;
+    cin >> s >> a >> b >> x;
+    ll onego = a + b;
+    ll totalInOnego = x / onego;
+    ll remaining = x % onego;
+    ll res = totalInOnego * s * a + s * min(remaining, a);
+    cout << res << endl;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
     int tc = 1;
     // cin >> tc;
-    for (int t = 1; t <= tc; t++) {
+    for (int t = 1; t <= tc; t++)
+    {
         // cout << "Case #" << t << ": ";
         solve();
     }
